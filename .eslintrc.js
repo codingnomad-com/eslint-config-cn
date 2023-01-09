e37ed36571912d0dd090bdf51e6c29f064e4785d
Manuel Bachl <manuel@coding-nomad.com>
2023-01-09 18:17:58 +0800

    Added naming convention rules

---
+++ .eslintrc.js
@@ -0,0 +1,118 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    parserOptions: {
+      project: ['./App.tsx', './tsconfig.json'],
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'react-hooks',
+    'prettier',
+    'import',
+    'jest',
+    'typescript-sort-keys',
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['strictCamelCase'],
+      },
+
+      {
+        selector: ['variable', 'function'],
+        format: ['strictCamelCase', 'PascalCase'],
+      },
+      {
+        selector: 'parameter',
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'require',
+      },
+
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'jest/no-export': 'off',
+    'jsx-a11y/label-has-associated-control': 0,
+    'no-console': 1,
+    'no-useless-constructor': 0,
+    'object-curly-newline': 'off',
+    'prefer-destructuring': 1,
+    'prettier/prettier': 1,
+    'react/destructuring-assignment': 1,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-props-no-spreading': 0,
+    'react/no-array-index-key': 0,
+    'react/no-unstable-nested-components': 'off',
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 0,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      // 'babel-module': {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # eslint-config-cn
+
+## Usage
+
+### Installation
+
+```bash
+yarn add @codingnomad/eslint-config-cn
+```
+
+### .eslintrc.js
+
+```js
+module.exports = {
+  extends: '@codingnomad/eslint-config-cn',
+  rules: {
+    ...
+  },
+};
+
+```
+
+### commitlint.config.js
+
+```js
+module.exports = {
+  extends: ['@codingnomad/eslint-config-cn'],
+};
+```
+
+### prettier.config.js
+
+```js
+module.exports = require('@codingnomad/eslint-config-cn/prettier.config');
+```
+
+### stylelint.config.js
+
+```js
+module.exports = {
+  extends: ['@codingnomad/eslint-config-cn'],
+};
+```
+
+### tsconfig.json
+
+```json
+TBD
+```
